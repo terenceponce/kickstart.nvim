e269159b572faeb933367c84a3a9b539dfb57fe3
Terence Ponce <terenceponce@gmail.com>
2023-04-03 21:50:25 +0100

    Add other languages on Treesitter for Phoenix development (#8)

--- init.lua
+++ init.lua
@@ -278,7 +278,25 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'elixir', 'ruby' },
+  ensure_installed = {
+    'c',
+    'cpp',
+    'go',
+    'lua',
+    'python',
+    'rust',
+    'tsx',
+    'typescript',
+    'vimdoc',
+    'vim',
+    'ruby',
+    'erlang',
+    'elixir',
+    'eex',
+    'heex',
+    'html',
+    'css'
+  },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
