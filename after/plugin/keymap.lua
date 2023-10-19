12cce2fd37b8182d07b1a14c07facb87dcdb70ab
Terence Ponce <terenceponce@outlook.com>
2023-10-19 13:58:49 +0100

    Add nvim-spider

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -1,3 +1,28 @@
 vim.keymap.set('n', '<leader>nr', '<Cmd>Neotree reveal<CR>', { desc = "Reveal file in File Explorer" })
 vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = "Toggle File Explorer" })
 vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle, { desc = "Display undo history" })
+
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"w",
+	"<cmd>lua require('spider').motion('w')<CR>",
+	{ desc = "Spider-w" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"e",
+	"<cmd>lua require('spider').motion('e')<CR>",
+	{ desc = "Spider-e" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"b",
+	"<cmd>lua require('spider').motion('b')<CR>",
+	{ desc = "Spider-b" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"ge",
+	"<cmd>lua require('spider').motion('ge')<CR>",
+	{ desc = "Spider-ge" }
+)
