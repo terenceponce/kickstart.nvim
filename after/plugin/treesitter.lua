f2601be51c5400378bc4c032a0ee9b49d5d96f30
Terence Ponce <terenceponce@outlook.com>
2023-10-19 14:25:08 +0100

    Add additional languages for treesitter

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,27 @@
+vim.defer_fn(function()
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = {
+      'c',
+      'cpp',
+      'go',
+      'lua',
+      'python',
+      'rust',
+      'tsx',
+      'javascript',
+      'typescript',
+      'vimdoc',
+      'vim',
+      'bash',
+      'ruby',
+      'erlang',
+      'elixir',
+      'eex',
+      'heex',
+      'html',
+      'css'
+    },
+  }
+end, 0)
+
