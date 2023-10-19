49567978da39aa8eeb6a7225a0c04ca89ebd721b
Terence Ponce <terenceponce@outlook.com>
2023-10-19 13:44:02 +0100

    Add undotree

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>nr', '<Cmd>Neotree reveal<CR>', { desc = "Reveal file in File Explorer" })
 vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = "Toggle File Explorer" })
+vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle, { desc = "Display undo history" })
