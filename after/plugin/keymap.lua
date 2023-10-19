9033e7828d4eeb3cf380a2024e6e49943b89b202
Terence Ponce <terenceponce@outlook.com>
2023-10-19 13:31:42 +0100

    Add neo-tree
    This also adds some changes to the default config for neo-tree

---
+++ after/plugin/keymap.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>nr', '<Cmd>Neotree reveal<CR>', { desc = "Reveal file in File Explorer" })
+vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = "Toggle File Explorer" })
