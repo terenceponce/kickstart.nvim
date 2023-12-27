c4a856d123ca24a9e2f34a3b7d3ef096829a5e88
Terence Ponce <terenceponce@gmail.com>
2023-12-27 07:40:20 +0000

    Add command for copying current file path to clipboard

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('n', '<leader>nr', '<Cmd>Neotree reveal<CR>', { desc = "Reveal file in File Explorer" })
 vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = "Toggle File Explorer" })
 vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle, { desc = "Display undo history" })
+vim.keymap.set('n', '<leader>cp', vim.cmd.CpPath, { desc = "Copy current file path to clipboard" })
 
 vim.keymap.set(
 	{ "n", "o", "x" },
